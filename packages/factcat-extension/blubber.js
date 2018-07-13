2c6d5396593e8fa187679f167c90bd4155a47f44
Ravern Koh <ravernkoh@gmail.com>
2018-07-13 22:04:35 +0800

    Add content blocking and blubber module

---
+++ packages/factcat-extension/blubber.js
@@ -0,0 +1,3 @@
+module.exports.create = function (post) {
+    // TODO: Render to div with id of ${post.id}.
+};
--- packages/factcat-extension/posts.js
+++ packages/factcat-extension/posts.js
@@ -1,7 +1,10 @@
 const uuid = require('uuid/v4');
+const blubber = require('./blubber');
 
 const POST_SELECTOR = '._5pcr.userContentWrapper';
 const LINK_SELECTOR = 'a._52c6';
+
+const THRESHOLD = 0.5;
 
 const posts = [];
 let processed = 0;
@@ -16,7 +19,12 @@ module.exports.modified = function () {
         const post = extract(rawPosts[i]);
         posts.push(post);
 
-        const score = process(post);
+        if (post.type == 'link') {
+            process(post);
+            if (post.scores.total < THRESHOLD) {
+                block(post);
+            }
+        }
     }
 
     processed = posts.length;
@@ -25,13 +33,21 @@ module.exports.modified = function () {
 // Gets various scores from each attribute and evaluates them. The closer to 0
 // a score is, the more fake it is.
 function process(post) {
-    return 0;
+    post.scores = { total: 0 };
+}
+
+// Blocks a post.
+function block(post) {
+    const root = post.raw.parentNode.parentNode.parentNode;
+    root.innerHTML = `<div id="${post.id}">BOOM</div>`;
+    blubber.create(post);
 }
 
 // Extracts relevant information out of a raw post element.
 function extract(rawPost) {
     const post = {
         id: uuid(),
+        raw: rawPost,
     };
 
     const a = rawPost.querySelector(LINK_SELECTOR);
