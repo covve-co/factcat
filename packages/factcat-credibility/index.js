534a0c558c5a6f2ff9506cd6bf0732f60b965924
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-07-13 23:36:11 +0800

    Fix regex

--- packages/factcat-credibility/index.js
+++ packages/factcat-credibility/index.js
@@ -1,11 +1,9 @@
 var osmosis = require('osmosis');
 
 let reputableDomain = new RegExp('(www.|https:\/\/).*([^\.]+)(.gov.sg|.edu.sg)');
-let reputableSource = new RegExp('(https?:\/\/(.+?\.)?(straitstimes|tnp|todayonline|businesstimes|zaobao|beritaharian|channelnewsasia|theindependent)\.(com|sg)(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&\'\(\)\*\+,;\=]*)?)');
+let reputableSource = new RegExp("(https?:\/\/(.+?\.)?(straitstimes|tnp|todayonline|businesstimes|zaobao|beritaharian|channelnewsasia|theindependent)\.(com|sg)(\/*)?)");
 
-// TEMP building as script
-let build = async (link) => {
-    let sentiment = 0.0;
+module.exports = async (link) => {
     console.log(`🔎 Forming graph for link ${link}`);
     // Verifies if govt 
     if (link.match(reputableDomain)) {
@@ -15,23 +13,21 @@ let build = async (link) => {
 
     if (link.match(reputableSource)) return 0.5;
 
-    return 0;
+    return 0.3;
 
     // Return score
 }
 
-async function scrape(link) {
-    let links = [];
-    await osmosis.get(link)
-        .find('a')
-        .set({
-            'link': '@href'
-        })
-        .data((listing) => {
-            links.push(listing.link);
-        })
-        .log(console.log);
-    return (links);
-}
-
-build("https://www.gov.sg/factually/all/");
+// async function scrape(link) {
+//     let links = [];
+//     await osmosis.get(link)
+//         .find('a')
+//         .set({
+//             'link': '@href'
+//         })
+//         .data((listing) => {
+//             links.push(listing.link);
+//         })
+//         .log(console.log);
+//     return links;
+// }
